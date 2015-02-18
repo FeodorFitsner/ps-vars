6e3350ba49dde5e973020e17ecac47b16337ca29
Feodor Fitsner <feodor@appveyor.com>
2015-02-18 11:54:35 -0800

    Added semi-colon

--- ci/AppveyorDeploy.ps1
+++ ci/AppveyorDeploy.ps1
@@ -1,7 +1,7 @@
-$accountName = $env:APPVEYOR_PROJECT_NAME
-$projectSlug = $env:APPVEYOR_PROJECT_SLUG
+$accountName = $env:APPVEYOR_PROJECT_NAME;
+$projectSlug = $env:APPVEYOR_PROJECT_SLUG;
 $projectBranch = $env:APPVEYOR_REPO_BRANCH
-$token = $env:ci_api_token
+$token = $env:ci_api_token;
 
 $lastSha = .\CI\GetLastSha.ps1 -accountName $accountName -projectSlug $projectSlug -projectBranch $projectBranch -token $token
 
